0bc62e8b750162fb68942905a050755b9c5dbad9
Ester Trevisan <ester.trevisan@academico.ifpb.edu.br>
2024-08-24 17:00:53 -0300

    :recycle: refactor: mudando forma como são processados caminhos de imagens do projeto

---
+++ gerar-pdf-itext/src/main/java/com/example/geradorDePDF/image/Imagens.java
@@ -0,0 +1,8 @@
+package com.example.geradorDePDF.image;
+
+public class Imagens {
+	
+	public static String LOGO_IFPB = Imagens.class.getResource("/imagens/LogoIfpb.jpeg").toString();
+	public static String LOGO_IFPB_CAMPUS_MONTEIRO = Imagens.class.getResource("/imagens/LogoIfpb_Monteiro.png").toString();
+
+}
--- gerar-pdf-itext/src/main/java/com/example/geradorDePDF/image/MainImage.java
+++ gerar-pdf-itext/src/main/java/com/example/geradorDePDF/image/MainImage.java
@@ -2,25 +2,27 @@ package com.example.geradorDePDF.image;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.PdfWriter;
 
 public class MainImage {
-    public static void main(String[] args) {
-        Document document = new Document();
+	
+    public static void main(String[] args) throws URISyntaxException, IOException {
+    	
+    	Document document = new Document();
         try {
-            PdfWriter.getInstance(document, new FileOutputStream("Imagem.pdf"));
+            PdfWriter.getInstance(document, new FileOutputStream("ImagemIfpbMonteiro.pdf"));
             document.open();
 
-            // Adicionando imagem de exemplo (substitua "path/to/logo.png" pelo caminho real da imagem)
-            ImageUtils.addImage(document, "path/to/logo.png");
-
+            ImageUtils.addImage(document, Imagens.LOGO_IFPB_CAMPUS_MONTEIRO);
+            
         } catch (DocumentException | IOException e) {
             e.printStackTrace();
         } finally {
-            document.close();
+        	document.close();
         }
     }
 }
